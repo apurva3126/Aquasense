a9e98c8701b0979d6155f6d9ef63f7d8d322a464
apurva3126 <apurva3073@gmail.com>
2026-02-20 12:13:09 +0530

    fgd

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://localhost:8000/api/v1";
+const API_BASE = "https://aquasensebackend-ru2t.onrender.com/api/v1";
 
 export async function getCrops() {
   const res = await fetch(`${API_BASE}/crops`);
